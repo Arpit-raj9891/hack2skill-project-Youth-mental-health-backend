199d0007bfe1a930dd9c43fb9b30cc190e377826
Arpit-raj9891 <arpitraj9891@gmail.com>
2025-09-21 12:21:34 +0530

    Add files via upload

---
+++ hack2skill-project-Youth-mental-health-backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from routes.chatbot import chatbot_bp
+from routes.journal import journal_bp
+
+app = Flask(__name__)
+
+# Register Blueprints (modular routes)
+app.register_blueprint(chatbot_bp, url_prefix="/chatbot")
+app.register_blueprint(journal_bp, url_prefix="/journal")
+
+@app.route("/")
+def home():
+    return {"message": "Hack2Skill Project Backend Running"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
