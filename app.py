b56a3882a3d314288743af4cff19d23499b8ae33
Vibhor61 <157367869+Vibhor61@users.noreply.github.com>
2025-09-21 14:39:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,5 @@ def home():
     return {"message": "Hack2Skill Project Backend Running"}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
